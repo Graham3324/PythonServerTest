d329a40cdf390f5569c3351299336e95fb61ba78
BuildTools <unconfigured@null.spigotmc.org>
2020-04-07 07:54:04 -0400

    Changes

---
+++ start.py
@@ -0,0 +1,16 @@
+import requests
+import sys
+
+
+r = requests.get("http://www.yahoo.com")
+p = requests.get
+
+
+def passer():
+    pass
+
+
+print(sys.version)
+print(sys.executable)
+
+print(r.status_code)
